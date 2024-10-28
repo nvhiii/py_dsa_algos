ed22ab2130ac4f0b0dff68af2ba2f3eb1192d83c
nvhiii <nahikhan16@gmail.com>
2024-10-28 12:13:27 -0400

    ss

---
+++ ch2/selection_sort.py
@@ -0,0 +1,22 @@
+def find_smallest(arr: list):
+    smallest = arr[0]
+    smallest_idx = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_idx = i
+    return smallest_idx
+
+def selection_sort(arr: list):
+    new = []
+    for i in range(len(arr)):
+        smallest_idx = find_smallest(arr) # get smallest idx
+        new.append(arr.pop(smallest_idx)) # removes smallest from array, and adds to new, (pop returns removed item)
+    return new
+
+def main():
+    my_list = [5, 3, 6, 2, 10]
+    print(selection_sort(my_list))
+
+if __name__ == "__main__":
+    main()
