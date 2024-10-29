b6765ddac1a30a62bb8288ba6a7e5585924420a2
nvhiii <nahikhan16@gmail.com>
2024-10-28 21:01:34 -0400

    qsort

---
+++ ch4/q_sort.py
@@ -0,0 +1,12 @@
+def q_sort(nums: list):
+    # dc
+    if len(nums) < 2:
+        return nums
+    else:
+        pivot = nums[0]
+        less = [i for i in nums[1:] if i <= pivot]
+        more = [i for i in nums[1:] if i > pivot]
+        return q_sort(less) + [pivot] + q_sort(more)
+    
+nums = [1, 2, 4, 6, 7, 4, 4213, 4, -1, 3]
+print(q_sort(nums))
