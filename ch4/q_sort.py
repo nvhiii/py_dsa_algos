63c7c6e46e9687cc280ad6cf606239c0c934f9a5
nvhiii <nahikhan16@gmail.com>
2024-10-30 18:44:41 -0400

    qsort

--- ch4/q_sort.py
+++ ch4/q_sort.py
@@ -1,13 +1,17 @@
-# O(nlogn) solution, n items in list * logn layers
-def q_sort(nums: list):
-    # dc
+# in order to get best run time, always have random pivot
+import random
+def quicksort(nums: list):
     if len(nums) < 2:
         return nums
     else:
-        pivot = nums[0]
-        less = [i for i in nums[1:] if i <= pivot]
-        more = [i for i in nums[1:] if i > pivot]
-        return q_sort(less) + [pivot] + q_sort(more)
-    
-nums = [1, 2, 4, 6, 7, 4, 4213, 4, -1, 3]
-print(q_sort(nums))
+        # pivot is a single number, hence its already "sorted"
+        index = random.randint(0, len(nums) - 1)
+        pivot = nums[index]
+        # make sure not to include pivot in these arrays
+        less = [num for i, num in enumerate(nums) if num <= pivot and i != index]
+        more = [num for i, num in enumerate(nums) if num > pivot and i != index]
+        return quicksort(less) + [pivot] + quicksort(more)
+        # basically creates huge call stack of pivots, then pops in end to sort
+
+my_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(quicksort(my_arr))
