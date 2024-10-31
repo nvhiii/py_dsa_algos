a356eb4d993fa3d2db6626873baf78ceb262cd38
nvhiii <nahikhan16@gmail.com>
2024-10-31 16:43:50 -0400

    bfs

---
+++ ch6/bfs.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+def search(name: str):
+    search_queue = deque()
+    search_queue += graph[name]
+    # check if we searched person already, dont need redundancy
+    searched = []
+    while search_queue:
+        person = search_queue.popleft() # returns first person to check against
+        if not person in searched:
+            if person_is_seller(person):
+                print(f"{person} is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False # if queue is empty, or person searched not found
+
+def person_is_seller(name: str):
+    return name[-1] == "m"
+
+print(search("you"))
