ed02f618cd25453f2e1f79034593041b61937c25
nvhiii <nahikhan16@gmail.com>
2024-11-10 23:41:40 -0500

    prog

---
+++ tamassia/ch2/arith_prog.py
@@ -0,0 +1,7 @@
+class ArithmeticProgression:
+    def __init__(self, increment=1, start=0):
+        super().__init__(start)
+        self._increment = increment
+
+    def _advance(self):
+        self._current += self._increment
---
+++ tamassia/ch2/geo_prog.py
@@ -0,0 +1,7 @@
+class GeometricProgression:
+    def __init__(self, base, start=1):
+        super().__init__(start)
+        self._base = base
+    
+    def _advance(self):
+        self._current *= self._base
