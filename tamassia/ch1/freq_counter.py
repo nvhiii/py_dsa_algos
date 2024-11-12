864a0609b5051cb6411b81e53a4b6dfcac6c51e0
nvhiii <nahikhan16@gmail.com>
2024-11-12 11:50:54 -0500

    fq

--- tamassia/ch1/freq_counter.py
+++ tamassia/ch1/freq_counter.py
@@ -1,59 +1,34 @@
 from collections import deque, Counter
 
-# def freq_counter(my_str: str, window_size: int):
-#     windows = [] # store all the resulting windows here
-#     # queue
-#     window = deque(my_str[:window_size])
-#     windows.append(dict(Counter(window)))
-#     for i in range(window_size, len(my_str)):
-#         removed_char = window.popleft()
-
-#         added_char = my_str[i]
-#         window.append(added_char)
-
-#         curr_freq = windows[-1].copy() # copies prev freq dict
-
-#         if curr_freq[removed_char] == 1:
-#             del curr_freq[removed_char]
-#         else:
-#             curr_freq[removed_char] -= 1
-
-#         if added_char in curr_freq:
-#             curr_freq[added_char] += 1
-#         else:
-#             curr_freq[added_char] = 1
-
-#         windows.append(curr_freq)
-
-#     return windows
-
-def freq_counter(my_str: str, window_size: int):
-    windows = []
-    window = deque(my_str[:window_size])
-    windows.append(dict(Counter(window)))
+def window_freq(my_str: str, window_size: int):
+    windows_n_size = []
+    first = deque(my_str[:window_size])
+    windows_n_size.append(dict(Counter(first)))
     for i in range(window_size, len(my_str)):
-        # remove last chara from last batch
-        prev_trailing_chara = window.popleft()
+        # get rid of first chara
+        first_char = first.popleft()
         # add next chara
-        next_chara = my_str[i] # get from iteration
-        window.append(next_chara) # append next chara
+        next_chara = my_str[i]
+        first.append(next_chara)
 
-        # copy last freq chart to do less work
-        curr_freq = windows[-1].copy()
+        # copy prev freq, its now current freq
+        curr = windows_n_size[-1].copy()
 
-        if curr_freq[prev_trailing_chara] == 1: # if its only 1, its not repeated so we delete
-            del curr_freq[prev_trailing_chara]
+        # first check if chara removed is in curr freq
+        if curr[first_char] == 1:
+            del curr[first_char]
         else:
-            curr_freq[prev_trailing_chara] -= 1
+            curr[first_char] -= 1
 
-        if next_chara in curr_freq:
-            curr_freq[next_chara] += 1
+        # next check if next chara already in curr freq
+        if next_chara in curr:
+            curr[next_chara] += 1
         else:
-            curr_freq[next_chara] = 1
+            curr[next_chara] = 1
 
-        windows.append(curr_freq)
+        windows_n_size.append(curr)
 
-    return windows
+    return windows_n_size
 
-print(freq_counter("hello", 3))
+print(window_freq("hello", 3))
 
