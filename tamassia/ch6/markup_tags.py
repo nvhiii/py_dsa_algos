bbf8a0f5fcaf1070b97d918d2ea67f1e9a7fce85
nvhiii <nahikhan16@gmail.com>
2024-11-15 18:21:05 -0500

    delim

---
+++ tamassia/ch6/markup_tags.py
@@ -0,0 +1,13 @@
+# from arraystack import ArrayStack
+
+# def markup_tags(file):
+#     opening = "<"
+#     closing = "/>"
+#     stack = ArrayStack()
+#     try:
+#         with open(file, "r") as html:
+#             for line in html:
+#                 line = line.strip()
+
+#     except FileNotFoundError:
+#         print(f"File {file} not found")
--- tamassia/ch6/match_delim.py
+++ tamassia/ch6/match_delim.py
@@ -4,12 +4,12 @@ def matched(expr):
     lefts = "{(["
     rights = "})]"
     stack = ArrayStack()
-    for bracket in expr:
-        if bracket in lefts:
-            stack.push(bracket)
-        elif bracket in rights:
+    for char in expr:
+        if char in lefts:
+            stack.push(char)
+        elif char in rights:
             if stack.is_empty(): # false bc cannot have right bracket before a corresponding left bracket
                 return False
-            if rights.index(bracket) != lefts.index(bracket): # if index in the string isnt same, it is mismatched
+            if rights.index(char) != lefts.index(char): # if index in the string isnt same, it is mismatched
                 return False
     return stack.is_empty()
