01f2edc5ada3a4b1c60bc0b8470597ea226db067
nvhiii <nahikhan16@gmail.com>
2024-11-15 18:05:05 -0500

    rstac

--- tamassia/ch5/scoreboard.py
+++ tamassia/ch5/scoreboard.py
@@ -1,10 +1,10 @@
-# class Scoreboard:
-#     def __init__(self, capacity=10):
-#         self._board = [None] * capacity
-#         self._n = 0
+    # class Scoreboard:
+    #     def __init__(self, capacity=10):
+    #         self._board = [None] * capacity
+    #         self._n = 0
 
-#     def __getitem__(self, k):
-#         return self._board[k]
-    
-#     def __str__(self):
-#         return '\n'.join(str(self._board[j]) for j in range(self._n))
+    #     def __getitem__(self, k):
+    #         return self._board[k]
+        
+    #     def __str__(self):
+    #         return '\n'.join(str(self._board[j]) for j in range(self._n))
---
+++ tamassia/ch6/reverse_file.py
@@ -0,0 +1,19 @@
+# reversefile.py
+from ArrayStack import ArrayStack  # Import ArrayStack
+
+def r_file(filename):
+    stack = ArrayStack()
+    try:
+        with open(filename, "r") as infile:
+            for line in infile:
+                stack.push(line.strip())
+    except FileNotFoundError:
+        print(f"File {filename} not found")
+        return  # Stop execution if file not found
+
+    with open(filename, "w") as outfile:
+        while not stack.is_empty():
+            outfile.write(stack.pop() + "\n")
+
+# Call the function to test
+r_file("pets.txt")
