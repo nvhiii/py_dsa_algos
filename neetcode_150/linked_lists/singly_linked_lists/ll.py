600d3eb14072a4727c85025dc5bc52689766a947
nvhiii <nahikhan16@gmail.com>
2024-11-19 22:20:40 -0500

    llptsd

--- neetcode_150/linked_lists/singly_linked_lists/ll.py
+++ neetcode_150/linked_lists/singly_linked_lists/ll.py
@@ -1,67 +1,91 @@
-# Design a Singly Linked List class.
-
-# Your LinkedList class should support the following operations:
-
-# LinkedList() will initialize an empty linked list.
-# int get(int i) will return the value of the ith node (0-indexed). If the index is out of bounds, return -1.
-# void insertHead(int val) will insert a node with val at the head of the list.
-# void insertTail(int val) will insert a node with val at the tail of the list.
-# bool remove(int i) will remove the ith node (0-indexed). If the index is out of bounds, return false, otherwise return true.
-# int[] getValues() return an array of all the values in the linked list, ordered from head to tail.
-
 class ListNode:
     def __init__(self, value, node = None):
         self.value = value
         self.next = node
 
-class LinkedList:
+class MyLinkedList(object):
+
     def __init__(self):
-        self.head = ListNode(-1)
+        self.head = ListNode(-1) # dummy for getting
         self.tail = self.head
 
-    # get
-    def get(self, i):
-        index = 0
-        curr = self.head.next # first non dummy val
+    def get(self, index):
+        """
+        :type index: int
+        :rtype: int
+        """
+        i = 0
+        curr = self.head.next # skip first idx
         while curr:
-            if index == i:
-                return curr.val
-            index += 1
+            if i == index:
+                return curr.value
+            i += 1
             curr = curr.next
-        return -1 # not found
-    
-    # insert head
-    def insertHead(self, val):
+        return -1
+        
+
+    def addAtHead(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
         new_node = ListNode(val)
         new_node.next = self.head.next
         self.head.next = new_node
         if not new_node.next:
             self.tail = new_node
 
-    # insert tail
-    def insertTail(self, val):
+    def addAtTail(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
         self.tail.next = ListNode(val)
         self.tail = self.tail.next
+        
 
-    # remove
-    def remove(self, i): # index
+    def addAtIndex(self, index, val):
+        """
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+        i = 0
         curr = self.head
-        index = 0
-        while curr and index < i:
-            index += 1
-            curr = curr.next # iterate until node before
+        while curr and i < index:
+            i += 1
+            curr = curr.next # get up to node before the indexed node
 
-        if curr and curr.next:
+        if curr: # curr is not None
+            new_node = ListNode(val)
+            new_node.next = curr.next
+            curr.next = new_node
+            if not new_node.next:
+                self.tail = new_node
+    
+    def deleteAtIndex(self, index):
+        """
+        :type index: int
+        :rtype: None
+        """
+        i = 0
+        curr = self.head
+        while curr and i < index:
+            i += 1
+            curr = curr.next
+
+        if curr and curr.next: # both must be valid
             if curr.next == self.tail:
                 self.tail = curr
-            curr.next = curr.next.next
+            curr.next = curr.next.next # removes the pointer to direct next node
             return True
         return False
-    
-    def getValues(self):
-        res = []
-        curr = self.head.next
-        while curr:
-            res.append(curr.val)
-            curr = curr.next
-        return res
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
