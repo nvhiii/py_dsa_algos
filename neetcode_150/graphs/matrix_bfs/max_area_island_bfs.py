bd849a594be24e58bbb5ec352d7a4d390f51cab6
nvhiii <nahikhan16@gmail.com>
2024-12-05 21:35:08 -0500

    max area island

---
+++ neetcode_150/graphs/matrix_bfs/max_area_island_bfs.py
@@ -0,0 +1,27 @@
+# from collections import deque
+
+# def maxAreaIsland(grid):
+#     ROWS, COLS = len(grid), len(grid[0])
+#     visited = set()
+#     q = deque()
+
+#     def bfs(r, c):
+#         q.append((r, c))
+#         visited.add((r, c))
+#         area = 0
+#         while q:
+#             r, c = q.popleft()
+#             if min(r, c) < 0 or r >= ROWS or c >= COLS or (r,c) in visited or grid[r][c] == 0:
+#                 continue
+#             q.append((r,c))
+#             visited.add((r,c))
+#             area += 1
+
+#         return area
+    
+#     area = 0
+#     for r in range(ROWS):
+#         for c in range(COLS):
+#             area = max(area, bfs(r, c))
+
+#     return area
--- neetcode_150/graphs/matrix_dfs/max_area_island.py
+++ neetcode_150/graphs/matrix_dfs/max_area_island.py
@@ -1,21 +1,19 @@
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        visited = set()
+# dfs solution
+def maxAreaOfIsland(grid):
+    ROWS, COLS = len(grid), len(grid[0])
+    visited = set()
 
-        def dfs(r, c):
-            if min(r, c) < 0 or r >= ROWS or c >= COLS or grid[r][c] == 0 or (r,c) in visited:
-                return 0
+    def dfs(r, c):
+        if min(r, c) < 0 or r >= ROWS or c >= COLS or (r,c) in visited or grid[r][c] == 0:
+            return 0
+        
+        visited.add((r, c))
 
-            # add to visited
-            visited.add((r, c))
+        return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+    
+    area = 0
+    for r in range(ROWS):
+        for c in range(COLS):
+            area = max(area, dfs(r, c))
 
-            return (1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c - 1) + dfs(r, c + 1))
-
-        # calc area
-        area = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                area = max(area, dfs(r, c))
-
-        return area
+    return area
