74141d18353ab3f8261a3d7025a151bcd4c6c042
nvhiii <nahikhan16@gmail.com>
2024-12-10 15:48:34 -0500

    cmts

--- neetcode_150/backtracking/subsets.py
+++ neetcode_150/backtracking/subsets.py
@@ -1,17 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # power set of a list of nums
+        # empty list returns empty list
+        # hard part is keeping track of same subsets, basically reordered
+
+        # [1] = [[], [1]]
+
+        # brute force:
+        # go through each item in the list, then branch out n times for the n number of items in list, this solution doesnt account for uniques tho
+        # one way is to back track optimize, so include branch to include curr node val or not.
+        # dfs?
+        # base case: if i >= len(nums), nothing to iterate, so we append copy of subset then return
+        # then append, then do dfs w paths of branch
+
         result = []
         subset = []
-        def dfs(i): # i is the index
-            if i >= len(nums): # out of bounds
+        def dfs(i):
+            if i >= len(nums):
                 result.append(subset.copy())
                 return
-            
-            subset.append(nums[i])
-            dfs(i + 1) # here, we run dfs on the tree that includes the num at ith index for the subset
 
-            subset.pop() # removed the nums[i] from the subset, hence [] branch
-            dfs(i + 1)
+            subset.append(nums[i])
+            dfs(i + 1) # curr valid path
+
+            subset.pop()
+            dfs(i + 1) # skipped
 
         dfs(0)
         return result
