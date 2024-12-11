a412abffef29859be6b2723add8faa2872ec1f72
nvhiii <nahikhan16@gmail.com>
2024-12-10 19:24:12 -0500

    ss

--- neetcode_150/heaps_pq/heapify/last_stone_weight.py
+++ neetcode_150/heaps_pq/heapify/last_stone_weight.py
@@ -1,16 +1,26 @@
-import heapq
-
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones = [-s for s in stones]
-        heapq.heapify(stones)
+        # notes
+        # stones arr w/ weights
+        # 2 heaviest stones per iter
+        # case to smash
+        # want to return 1 stone at end (or 0)
+        # mismatch = always smash and append
+
+        # brute force
+        # sort the stones arr, then reverse iterate from end. O(n) operation
+        # then we append too, o(1) tho.
+        # could be better, say we use min heap
+        # o(n) for heapify
+        # then we access in logn
+
+        stones = [-s for s in stones] # mimic max heap using negation
+        heapq.heapify(stones) 
+
         while len(stones) > 1:
-            f = heapq.heappop(stones)
-            s = heapq.heappop(stones)
-            if s > f:
-                heapq.heappush(stones, f - s)
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
+            if x != y:
+                heapq.heappush(stones, x - y) # we append x - y bc the values are negated in our arr
 
-        if not stones:
-            stones.append(0)
-
-        return abs(stones[0])
+        return -stones[0] if stones else 0
