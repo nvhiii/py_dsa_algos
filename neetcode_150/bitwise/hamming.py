ca5296217a3c01a9bffacaf885dda81d196963cd
nvhiii <nahikhan16@gmail.com>
2024-12-12 15:03:02 -0500

    some revision

--- neetcode_150/bitwise/hamming.py
+++ neetcode_150/bitwise/hamming.py
@@ -1,10 +1,10 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # return num of bits given a int
+        # given that its a 32 bit number
+        # give num bits in this number
         count = 0
         while n > 0:
-            if n & 1 == 1: # truthy bc we check if last val is a 1, 2^0
+            if n & 1 == 1: # valid case, same bit at end
                 count += 1
-            # decrement the num by a factor of 2,  bitwise move right /2
-            n = n >> 1
+            n = n >> 1 # divide by 2 to the power of 1
         return count
